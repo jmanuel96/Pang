311972ad4d96c127d2fc06956015dd327fd01c6d
josemanuel <josemanuel@JoseManuel>
2019-01-17 10:35:28 +0100

    17 Enero 2019

--- src/es/josemanuelcarrasco/pangfx/PangFX.java
+++ src/es/josemanuelcarrasco/pangfx/PangFX.java
@@ -26,6 +26,8 @@ public class PangFX extends Application {
         
         int ancho = 1366;
         int alto = 768;
+        
+        // Elementos del borde de la pantalla del juego
         Rectangle ContornoVertical = new Rectangle(0, 0, 15, 600);
         ContornoVertical.setFill(Color.GREENYELLOW);
         Rectangle ContornoVertical2 = new Rectangle(1349,0,1366,600);
@@ -34,11 +36,43 @@ public class PangFX extends Application {
         ContornoHorizontal.setFill(Color.GREENYELLOW);
         Rectangle ContornoHorizontal2 = new Rectangle(0,600,1366,15);
         ContornoHorizontal2.setFill(Color.GREENYELLOW);
+        
+        // Elementos del Cielo del juego
         Rectangle Cielo = new Rectangle(15,15,1334,585);
         Cielo.setFill(Color.LIGHTSKYBLUE);
+        
+        // Elementos del Cajetin del juego
         Rectangle Cajetin = new Rectangle (0,615,1366,110);
-        Cajetin.setFill(Color.SLATEGRAY);
-        Circle Nube1 = new Circle(160, 200,30);   
+        Cajetin.setFill(Color.SLATEGRAY); 
+        
+        // Elementos de la montaña del juego
+        Polygon poligonomontaña = new Polygon (new double[]{
+            15.0, 600.0,
+            1349.0, 600.0,
+            600.0, 150.0,
+            450.0, 150.0});
+        
+        Polygon nievemontaña = new Polygon (new double[]{
+            600.0, 150.0,
+            450.0, 150.0,
+            363.0, 240.0,
+            751.0, 240.0});
+        
+        // Elementos para la creación de las nubes del juego
+        Circle elementonube1 = new Circle (200,150,25);
+        Circle elementonube2 = new Circle (225,150,20);
+        Circle elementonube3 = new Circle (175,150,18);
+        
+        // Colores de la Montaña
+        nievemontaña.setFill(Color.WHITE);
+        poligonomontaña.setFill(Color.DARKSLATEGREY);
+        
+        // Colores de las nubes
+        elementonube1.setFill(Color.WHITE);
+        elementonube2.setFill(Color.WHITE);
+        elementonube3.setFill(Color.WHITE);
+        
+        
         
         Pane root = new Pane();
         root.getChildren().add(ContornoVertical);
@@ -47,7 +81,11 @@ public class PangFX extends Application {
         root.getChildren().add(ContornoHorizontal2);
         root.getChildren().add(Cielo);
         root.getChildren().add(Cajetin);
-        root.getChildren().add(Nube1);
+        root.getChildren().add(poligonomontaña);
+        root.getChildren().add(nievemontaña);
+        root.getChildren().add(elementonube1);
+        root.getChildren().add(elementonube2);
+        root.getChildren().add(elementonube3);
         
         Scene scene = new Scene (root, ancho, alto);
         
